2e2dfcc4d90f90fd1e9a917d2a77e58a66bf6294
Jordane <jordan2offshel@hotmaile.com>
2022-10-29 10:45:03 -0400

    Sixth commit

--- public/js/main.js
+++ public/js/main.js
@@ -6,3 +6,13 @@ btn_menu.addEventListener("click", () => {
     btn_menu.classList.toggle("active");
     menu.classList.toggle("active")
 })
+
+var header_js = document.querySelector(".js-header");
+
+window.addEventListener("scroll", () => {
+    if (this.scrollY > 0) {
+        header_js.classList.add("active");
+    } else {
+        header_js.classList.remove("active");
+    }
+})
